8f658954bd4696296bd85e4b4035fca1ebf3c249
Sylvieho <sylvie.ho@univ-tlse3.fr>
2026-03-10 17:53:24 +0100

    3-2 méthode toString

--- classeIterable/src/cartes/Type.java
+++ classeIterable/src/cartes/Type.java
@@ -1,5 +1,30 @@
 package cartes;
 
 public enum Type {
-	FEU, ESSENCE, CREVAISON, ACCIDENT;
+	FEU("Feu rouge", "Feu vert", "Véhicule prioritaire"),
+	ESSENCE("Panne d'essence", "Essence", "Citerne d'essence"),
+	CREVAISON("Crevaison", "Roue de secours", "Increvable"),
+	ACCIDENT("Accident", "Réparations", "As du volant");
+	
+	private String attaque;
+	private String parade;
+	private String botte;
+	
+	private Type(String attaque, String parade, String botte) {
+		this.attaque = attaque;
+		this.parade = parade;
+		this.botte = botte;
+	}
+
+	public String getAttaque() {
+		return attaque;
+	}
+
+	public String getParade() {
+		return parade;
+	}
+
+	public String getBotte() {
+		return botte;
+	}
 }
