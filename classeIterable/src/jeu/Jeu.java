f4beef5814605a358da8de82c18e6d2cbfdf0e69
Sylvieho <sylvie.ho@univ-tlse3.fr>
2026-04-22 16:45:46 +0200

    tp4 partie 3

--- classeIterable/src/jeu/Jeu.java
+++ classeIterable/src/jeu/Jeu.java
@@ -2,7 +2,10 @@ package jeu;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import cartes.Carte;
 import cartes.JeuDeCartes;
@@ -10,6 +13,8 @@ import utils.GestionCartes;
 
 public class Jeu {
 	private Sabot sabot;
+	private LinkedHashSet<Joueur> joueurs = new LinkedHashSet<>();
+	private static final int NBCARTES = 6;
 	
 	public Jeu() {
 		JeuDeCartes jeu = new JeuDeCartes();
@@ -22,5 +27,19 @@ public class Jeu {
 		GestionCartes.melanger(listeCartes);
 		
 		sabot = new Sabot(listeCartes.toArray(new Carte[0]));
+	}
+	
+	public void inscrire(Set<Joueur> joueursInscrits) {
+		for(Joueur joueur : joueursInscrits) {
+			joueurs.addLast(joueur);
+		}
+	}
+	
+	public void distribuerCartes() {
+		for(int i = 0; i < NBCARTES; i++) {
+			for(Joueur joueur : joueurs) {
+				joueur.donner(sabot.piocher());
+			}
+		}
 	}
 }
