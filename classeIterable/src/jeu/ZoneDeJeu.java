2c092f7d8058c53d1e55095dd033e15d25634f9c
Sylvieho <sylvie.ho@univ-tlse3.fr>
2026-04-22 14:09:09 +0200

    fin tp3

--- classeIterable/src/jeu/ZoneDeJeu.java
+++ classeIterable/src/jeu/ZoneDeJeu.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import cartes.Attaque;
 import cartes.Bataille;
 import cartes.Borne;
 import cartes.Carte;
+import cartes.Cartes;
+import cartes.DebutLimite;
 import cartes.FinLimite;
 import cartes.Limite;
+import cartes.Parade;
+import cartes.Type;
  
 public class ZoneDeJeu {
 	private List<Limite> pileDeLimites = new ArrayList<>();
@@ -42,6 +47,58 @@ public class ZoneDeJeu {
 			pileDeLimites.add(limite);
 		} else if (c instanceof Bataille bataille) {
 			pileDeBatailles.add(bataille);
+		}
+	}
+	
+	public boolean peutAvancer() {
+		return !pileDeBatailles.isEmpty() && pileDeBatailles.getFirst().equals(Cartes.FEU_VERT);
+	}
+	
+	public boolean estDepotFeuVertAutorise() {
+		if (pileDeBatailles.isEmpty()) return true;
+		Bataille top = pileDeBatailles.getFirst();
+		return top.equals(Cartes.FEU_ROUGE) || (top instanceof Parade && !top.equals(Cartes.FEU_VERT));
+	}
+	
+	private boolean estDepotBorneAutorise(Borne borne) {
+		return peutAvancer() && (donnerLimitationVitesse() >= borne.getKm() 
+				|| donnerKmParcourus() + borne.getKm() <= 1000);
+	}
+	
+	private boolean estDepotLimiteAutorise(Limite limite) {
+		if (limite instanceof DebutLimite) {
+			return pileDeLimites.isEmpty() || pileDeLimites.getFirst() instanceof FinLimite;
+		} else if (limite instanceof FinLimite) {
+			return !pileDeLimites.isEmpty() && pileDeLimites.getFirst() instanceof DebutLimite;
+		}
+		return false;
+	}
+	
+	private boolean estDepotBatailleAutorise(Bataille bataille) {
+		if (bataille instanceof Attaque) {
+			return peutAvancer();
+		}
+		if(bataille instanceof Parade) {
+			if(bataille.getType() == Type.FEU) {
+				return estDepotFeuVertAutorise();
+			} else if(!pileDeBatailles.isEmpty()){
+				Bataille sommet = pileDeBatailles.getFirst();
+				return sommet instanceof Attaque && sommet.getType() == bataille.getType();
+			}
+		}
+		return false;
+	}
+	
+	public boolean estDepotAutorise(Carte carte) {
+		switch(carte) {
+			case Borne borne:
+				return estDepotBorneAutorise(borne);
+			case Limite limite :
+				return estDepotLimiteAutorise(limite);
+			case Bataille bataille : 
+				return estDepotBatailleAutorise(bataille);
+			default : 
+				return true;
 		}
 	}
 }
